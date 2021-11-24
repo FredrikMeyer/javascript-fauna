ab7451ab93a2573e04845ea73a14cbb72a0a17ea
Fredrik Meyer <hrmeyer@gmail.com>
2021-11-24 22:38:40 +0100

    Add cheat sheet + move fetcher to helper file

---
+++ example-3-production-app/src/helper.ts
@@ -0,0 +1,42 @@
+export interface ResponseType {
+  properties: {
+    meta: {
+      units: Record<string, string>;
+    };
+    timeseries: {
+      time: string;
+      data: { instant: { details: Record<string, number> } };
+    }[];
+  };
+}
+
+/**
+ * Fetch forecast from met.no
+ *
+ * @param lat Latitude
+ * @param lon Longitude
+ * @return Either the JSON response or undefined.
+ */
+export async function fetchForecast(
+  lat: number,
+  long: number
+): Promise<ResponseType | undefined> {
+  const queryParams = new URLSearchParams({
+    lat: lat.toString(),
+    lon: long.toString(),
+  });
+  const response = await fetch(
+    `https://api.met.no/weatherapi/locationforecast/2.0/complete.json?${queryParams.toString()}`,
+    {
+      headers: {
+        "User-Agent": "fredrik meyer",
+      },
+    }
+  );
+
+  try {
+    return response.json();
+  } catch (e) {
+    console.error(e);
+  }
+}
