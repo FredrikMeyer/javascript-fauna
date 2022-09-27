807531eaaedbb9b867406abe36af5b0ca98be042
Fredrik Meyer <hrmeyer@gmail.com>
2022-09-27 18:11:47 +0200

    Skip libcheck

--- example-3-production-app/src/main.tsx
+++ example-3-production-app/src/main.tsx
@@ -1,18 +1,22 @@
 import React from "react";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 
 const mdTheme = createTheme();
 
-ReactDOM.render(
-  <React.StrictMode>
-    <ThemeProvider theme={mdTheme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
-);
+const container = document.getElementById("root");
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <ThemeProvider theme={mdTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </React.StrictMode>
+  );
+}
